c687cb2a31fbce6e9b794a5b7cd62b68c4097ed4
David Martínez Cascante <dawidh15@gmail.com>
2021-02-16 09:36:14 -0600

    first commit david branch

---
+++ Chapter 05/Alter Customer Table for exercises.sql
@@ -0,0 +1,18 @@
+﻿-- Add new ID column to avoid the PK problem
+
+-- Get the name of a constraint into a variable
+DECLARE @CustomerPKName AS VARCHAR(100);
+
+SET @CustomerPKName =  (SELECT CONSTRAINT_NAME
+						  FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+						  WHERE TABLE_CONSTRAINTS.TABLE_NAME = 'Customer'
+						  AND CONSTRAINT_TYPE = 'PRIMARY KEY');
+
+EXEC('ALTER TABLE Customer DROP CONSTRAINT ' + @CustomerPKName);
+
+GO
+
+-- Add a new auto increment col and set it to PK
+ALTER TABLE Customer ADD  ID INT IDENTITY(1,1);
+
+ALTER TABLE CUSTOMER ADD CONSTRAINT PK_Customer PRIMARY KEY(ID);
